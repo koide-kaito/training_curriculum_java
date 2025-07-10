cd83a7d3522f2d03a9cdf896688eaf8cc775391b
koide-kaito <koide.kaito@minebea-ss.com>
2025-07-10 15:45:13 +0900

    Issue2

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -57,7 +57,8 @@ public class CalendarsController {
 
     for (int x = 0; x < 7; x++) {
       // Map<String, Object> day_map = new HashMap<String, Object>();
-      Map<String, Object> day_map = new HashMap<>();
+      // Map<String, Object> day_map = new HashMap<>();
+      Map<String, Object> dayMap = new HashMap<>();
 
       LocalDate currentDate = todaysDate.plusDays(x);
 
@@ -68,11 +69,17 @@ public class CalendarsController {
           }
       }
 
-      day_map.put("month", currentDate.getMonthValue());
-      day_map.put("date", currentDate.getDayOfMonth());
-      day_map.put("plans", todayPlans);
+      // day_map.put("month", currentDate.getMonthValue());
+      // day_map.put("date", currentDate.getDayOfMonth());
+      // day_map.put("plans", todayPlans);
 
-      weekDays.add(day_map);
+      // weekDays.add(day_map);
+      
+      dayMap.put("month", currentDate.getMonthValue());
+      dayMap.put("date", currentDate.getDayOfMonth());
+      dayMap.put("plans", todayPlans);
+
+      weekDays.add(dayMap);
     }
 
     return weekDays;
